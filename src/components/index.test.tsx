0d871bcb87d2f9871c0fd7bf90c2b165a946a394
eddyzera <edgar.nascimento@newgo.com.br>
2022-07-11 23:27:19 -0300

    [fix]: problem when run test

---
+++ src/components/index.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { Hello } from '.'
+
+describe('Test Jest', () => {
+  it('should render component without errors', () => {
+    const { container } = render(<Hello />)
+    expect(container.querySelector('.hello-world')).toBeInTheDocument()
+  })
+})
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Hello: React.FunctionComponent = () => {
+  return <h1 className='hello-world'>Hello World</h1>
+}
