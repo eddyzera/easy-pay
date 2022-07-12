6af2d3a5e7800d5d4adef0ba5d7d7ed1ad08f572
eddyzera <edgar.nascimento@newgo.com.br>
2022-07-12 20:45:32 -0300

    [fix]: dowgrade version lib for run storybook

--- src/components/index.test.tsx
+++ src/components/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { Hello } from './Hello'
+import { Hello } from '.'
 
 describe('Test Jest', () => {
   it('should render component without errors', () => {
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import styles from './Hello.module.scss'
 
 export const Hello: React.FunctionComponent<{ label: string }> = ({ label }) => {
-  return <h1 className='hello-world'>{label}</h1>
+  return <h1 className={styles.hello}>{label}</h1>
 }
